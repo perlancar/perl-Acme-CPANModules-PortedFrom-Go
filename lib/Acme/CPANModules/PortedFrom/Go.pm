dc199809ebd6752555251d84fa63b981312e7c77
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-02-07 05:49:12 +0700

    Initial commit

---
+++ lib/Acme/CPANModules/PortedFrom/Go.pm
@@ -0,0 +1,27 @@
+package Acme::CPANModules::PortedFrom::Go;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+our $LIST = {
+    summary => "Modules/applications that are ported from (or inspired by) ".
+        "Go",
+    description => <<'_',
+
+If you know of others, please drop me a message.
+
+_
+    entries => [
+        {
+            module => 'Getopt::Kingpin',
+            summary => 'kingpin',
+        },
+    ],
+};
+
+1;
+# ABSTRACT:
+
+=head1 DESCRIPTION
